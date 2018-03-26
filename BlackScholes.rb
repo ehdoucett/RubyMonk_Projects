79ab4ce6ddebae8795f2d1d951ac5045c7c996ca
ehdoucett <evan.doucett@gmail.com>
2018-03-25 20:39:25 -0600

    Add files via upload

---
+++ BlackScholes.rb
@@ -0,0 +1,142 @@
+# Call Option Price Calculator in Ruby
+
+# Call(S, t) = N(d_1)*S - N(d_2)*K*exp(-r*(t))
+#	S = current price of stock
+#	t = time to maturity (years)
+#	K = strike price of option
+#	r = risk-free interest rate
+#	N(x) = cumulative distribution funciton of standard normal distribution
+#	d_1 = (log(S/K) + (r + sigma^2/2)*(t))/(sigma*sqrt(t))
+#	d_2 = d_1 - sigma*sqrt(t)
+#	can verify numbers with http://www.fintools.com/resources/online-calculators/options-calcs/options-calculator/
+
+class CallOption
+	attr_reader :s, :k, :r, :t, :sigma
+	def initialize(s,k,r,t,sigma)
+		# Initialize constants and input parameters
+		@RT2PI = Math.sqrt(4.0*Math.acos(0.0))
+		@SPLIT = 7.07106781186547
+		@N0 = 220.206867912376
+		@N1 = 221.213596169931
+		@N2 = 112.079291497871
+		@N3 = 33.912866078383
+		@N4 = 6.37396220353165
+		@N5 = 0.700383064443688
+		@N6 = 3.52624965998911e-02
+		@M0 = 440.413735824752
+		@M1 = 793.826512519948
+		@M2 = 637.333633378831
+		@M3 = 296.564248779674
+		@M4 = 86.7807322029461
+		@M5 = 16.064177579207
+		@M6 = 1.75566716318264
+		@M7 = 8.83883476483184e-02
+		@U1 = 0.0
+		@U2 = 0.0
+		@V1 = 0.0
+		@V2 = 0.0
+		@s = s
+		@k = k
+		@r = r
+		@t = t
+		@sigma = sigma
+	end
+
+	def N(x)
+		# calculate the cumulative normal distribution for given random variable x
+		# for x = 0, N(x) = 0.5
+		z = x.abs
+		c = 0.0
+		
+		if (z <= 37.0)
+			e = Math.exp(-z * z / 2.0)
+			if (z < @SPLIT)
+				n = (((((@N6*z + @N5)*z + @N4)*z + @N3)*z + @N2)*z + @N1)*z + @N0
+				d = ((((((@M7*z + @M6)*z + @M5)*z + @M4)*z + @M3)*z + @M2)*z + @M1)*z + @M0
+				c = e * n / d
+			else
+				f = z + 1.0 / (z + 2.0 / (z + 3.0 / (z + 4.0 / (z + 13.0 / 20.0))))
+				c = e / (@RT2PI * f)
+			end
+		end
+		return (x <= 0.0) ? c : 1 - c
+	end
+	
+	def ComputePriceBS
+		# calculate d_1 random variable
+		d_1 = (Math.log(s / k) + t * (r + sigma**2 / 2.0)) / (sigma * Math.sqrt(t)) 
+		
+		# calculate d_2 random variable
+		d_2 = d_1 - sigma * Math.sqrt(t)
+		
+		# return the price of the call option with given parameters
+		return (s * N(d_1) - k * Math.exp(-r * t) * N(d_2)).round(4)
+	end
+	
+	def Normal_Sample(mean = 0.0, sd = 1.0)
+		
+		sample = 0.0
+		loop do
+			if sample == 0.0 || sample >= 1.0
+				@U1 = rand
+				@U2 = rand
+				@V1 = 2.0 * @U1 - 1.0
+				@V2 = 2.0 * @U2 - 1.0
+				sample = @V1**2 + @V2**2
+			else
+				break
+			end
+		end
+		return ((@V1*Math.sqrt(-2.0 * Math.log(sample) / sample)) * sd) + mean
+	end
+	
+	def ComputePriceMC(m, n)
+		# calculates price using Monte Carlo simulation with m iterations on n sub-intervals
+		m_fl = m.to_f
+		n_fl = n.to_f 
+		c = 0.0
+		m.times do
+			price = s 
+			n.times do 
+				x = Normal_Sample()
+				p = Math.exp((r - sigma**2 / 2.0)*(t / n_fl) + (sigma * Math.sqrt(t / n_fl) * x))
+				price = price * p
+			end
+			c = c + [price - k, 0.0].max 
+		end
+		c = c * Math.exp(-r * t) / m_fl 
+		return c.round(4)
+	end
+	
+	def BestMC(threshold)
+		# method to determine reasonable number of MC iterations to use for price of option
+		# runs through different values for m, n to find price within price threshold of BS
+		bs_price = ComputePriceBS()
+		m = 1
+		n = 1
+		mc_price = ComputePriceMC(m, n)
+		loop do 
+			if (mc_price - bs_price).abs <= threshold || n > 20000
+				break
+			else
+				m = m * 2
+				mc_price = ComputePriceMC(m, n)
+				if (mc_price - bs_price).abs <= threshold
+					break
+				end
+				n = n * 2
+				mc_price = ComputePriceMC(m, n)
+				if (mc_price - bs_price).abs <= threshold
+					break
+				end
+			end
+		end
+		return [bs_price, mc_price, m, n, threshold]
+	end
+
+end
+
+myoption = CallOption.new(100.00,105.00,0.05,1.00,0.20) # create a new option object
+#puts myoption.ComputePriceBS # call function to compute price of option using Black Scholes model
+#puts myoption.ComputePriceMC(1000,1000) # call function to compute price of option using Monte-Carlo sampling method (might take a while)
+puts myoption.BestMC(0.15)
---
+++ RubyMonkProjects.rb
@@ -0,0 +1,269 @@
+# Build a Calculator
+
+class Calculator
+  def add(a, b)
+   # your code here
+    a+b
+  end
+
+  def subtract(a, b)
+   # your code here
+   a-b
+  end
+end
+
+
+# Find the Frequency of a String
+
+def find_frequency(sentence, word)
+  # Your code here
+  a=sentence.downcase.split
+  a.count(word)
+end
+
+
+# Sort the words in a given sentence
+
+def sort_string(string)
+  # your code here
+  a = string.split(" ")
+  b = a.sort{|x,y| x.length <=> y.length}
+  b[0]<<' '<<b[1]<<' '<<b[2]
+end
+
+
+# Select random elements from an array
+
+def random_select(array, n)
+  # your code here
+  len = array.size
+  result = []
+  n.times do 
+    result = result << array[rand(len)]    
+  end
+  return result
+end
+
+
+# Find the length of strings in an array
+
+def length_finder(input_array)
+  out_arr = []
+  input_array.each do |i|
+    out_arr = out_arr << i.length
+  end
+  return out_arr
+end
+
+
+# Hiring Programmers - Boolean Expressions in Ruby
+
+is_an_experienced_programmer = (
+  (candidate.years_of_experience >= 2 || candidate.github_points >= 500) && candidate.languages_worked_with.include?("Ruby") && candidate.age >= 15 && !candidate.applied_recently?
+  )
+  
+  
+# Palindromes  
+
+def palindrome?(sentence)
+  # Write your code here
+  sentence = sentence.downcase.gsub(' ','')
+  reverse = ''
+  i = 1
+  sentence.length.times do
+    reverse = reverse << sentence[-i]
+    i = i.next
+  end
+  return reverse == sentence
+end
+
+
+# Compute sum of cubes for given range
+
+def sum_of_cubes(a, b)
+  # Write your code here
+  i = a
+  result = i**3
+  loop do
+    if i == b 
+      break
+    else
+      i = i + 1
+      result = result + i**3
+    end
+  end
+    return result
+end
+ 
+ 
+# Find non-duplicate values in an Array
+
+def non_duplicated_values(values)
+  # Write your code here
+  out_arr = values
+  values.each do |x|
+    n = values.select {|y| y == x}
+    if n.length > 1
+      out_arr.delete_if {|y| y == x}
+    end
+  end
+  return out_arr
+end
+
+
+# Check if all elements in an array are Fixnum
+
+def array_of_fixnums?(array)
+  # Write your code here
+  n = 0
+  n = array.select {|x| x.is_a?(String) }
+  if n.length == 0
+    return true
+  else 
+    return false
+  end 
+end
+
+
+# Kaprekar's Number
+
+def kaprekar?(k)
+  k2 = k**2
+  n = "#{k}".length
+  num_digits_k2 = "#{k2}".length
+  right_n = k2 % (10**n)
+  left = k2 / (10**n)
+  k == (right_n + left)
+end
+
+
+# Number shuffle
+
+def number_shuffle(number)
+  # your code here
+  len = "#{number}".length
+  arr = []
+  i = 0
+  num = 1
+  len.times do 
+    x = number % 10**(i+1)
+    arr[i] = x / (10**i)
+    i = i + 1
+    num = num * i
+  end
+  result = []
+  loop do 
+    if result.length == num
+      break
+    else
+      x = arr.shuffle
+      y = 0
+      i = 10**(arr.length-1)
+      x.each do |k|
+        y = y + (k * i)
+        i = i / 10
+      end
+      result.push(y)
+      result = result.uniq
+    end
+  end
+    return result.sort
+end
+
+
+# Time to run code
+
+def exec_time(proc)
+  # your code here
+  h = Time.now
+  puts proc.call
+  n = Time.now
+    return n - h  
+end	
+
+
+# Your sum
+
+class MyArray
+  attr_reader :array
+
+  def initialize(array)
+    @array = array
+  end
+
+  def sum(initial_value = 0)
+    if block_given?
+      res = initial_value
+      array.each do |i|
+        res = res + yield(i)
+      end
+      return res
+    else
+      res = initial_value
+      array.each do |i|
+        res = res + i
+      end
+      return res
+    end
+  end
+end
+
+
+# Orders and costs
+
+class Restaurant
+  def initialize(menu)
+    @menu = menu
+  end
+
+  def cost(*orders)
+    # your code here
+    total = 0
+    orders.each do |ord|
+      ord.each do |i, q|
+        p = @menu[i]
+        c = p * q
+        total = total + c
+      end
+    end
+    return total
+  end
+end
+
+
+# Enough Contrast?
+
+class Color
+  attr_reader :r, :g, :b
+  def initialize(r, g, b)
+    @r = r
+    @g = g
+    @b = b
+  end
+
+  def brightness_index
+    # your code here
+    return ( 299 * r + 587 * g + 114 * b) / 1000
+  end
+
+  def brightness_difference(another_color)
+    #your code here
+    return (brightness_index - another_color.brightness_index).abs
+  end
+
+  def hue_difference(another_color)
+    #your code here
+    return ((r - another_color::r).abs + (g - another_color::g).abs + (b - another_color::b).abs)
+  end
+
+  def enough_contrast?(another_color)
+    # your code here
+    if brightness_difference(another_color) > 125 && hue_difference(another_color) > 500 
+      true
+    else
+      false
+    end
+  end
+end
+
+
